430915f6bc2b1fe9ee1af003220e81acdb97628a
awareautoh <ouphachay@yahoo.com>
2018-05-31 00:41:33 +0100

    first commit

---
+++ pow_function.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+//define function to power number
+
+
+ int pow(int base, int exponent){
+
+ 	
+ 	int result;
+ 	for(int i=0; i< exponent; i=i+1){
+
+ 		result *= base;	
+ 	}
+
+ 	
+
+ 	return result;
+ }
+
+	
+
+
+int main(){
+
+//revoke funtion 
+
+ int callfunc;
+
+ callfunc = pow(2,4);
+
+ cout<< callfunc <<endl;
+
+
+	
+
+
+
+
+return 0;
+
+}
