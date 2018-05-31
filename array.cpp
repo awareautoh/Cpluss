7eed7bd1344feeae1e58febc765c90be9fb1ad0f
awareautoh <ouphachay@yahoo.com>
2018-05-31 22:27:31 +0100

    change

--- array.cpp
+++ array.cpp
@@ -8,19 +8,19 @@ int main(){
 
 	int myArray[] = {1,2,3};
 
-	cout<< myArray[1] <<endl;
+	cout<< myArray[0] <<endl;
 
 
 
 
 	// multi dimention array 
 
-	/*int marray[2][2] = {1,2,3,4};
+	int marray[2][2] = {1,2,3,4};
 
 	cout<< "The array [0][0]is " << marray[0][0] <<endl;
 	cout<< "The array [0][1]is " << marray[0][1] <<endl;
 	cout<< "The array [1][0]is " << marray[1][0] <<endl;
-	cout<< "The array [1][1]is " << marray[1][1] <<endl;*/
+	cout<< "The array [1][1]is " << marray[1][1] <<endl;
 
 
 return 0;
