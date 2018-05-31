b7b297953b917ac29e2b8c3fa05ab98806a85683
awareautoh <ouphachay@yahoo.com>
2018-05-31 13:30:08 +0100

    first commit

---
+++ nested_loop.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main(){
+	
+	//neested loop to create triangle 
+
+	for(int i = 0; i<5; i++){
+		for(int x=0; x<5; x++){
+			cout<<" ";
+		}
+		for(int j =5-i; j>0; j--){
+			cout<<" ";
+		}
+		for(int k=0; k<2*i+1; k++){
+			cout<< "*";
+		}
+
+		cout<<endl;
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+	return 0;
+}
