ec0d5883b415620ec5ac00f0a477fc3f524f75d3
awareautoh <ouphachay@hotmail.com>
2018-05-31 21:06:24 +0100

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -15,12 +15,12 @@ int main(){
 
 	// multi dimention array 
 
-	/*int marray[2][2] = {1,2,3,4};
+	int marray[2][2] = {1,2,3,4};
 
 	cout<< "The array [0][0]is " << marray[0][0] <<endl;
 	cout<< "The array [0][1]is " << marray[0][1] <<endl;
 	cout<< "The array [1][0]is " << marray[1][0] <<endl;
-	cout<< "The array [1][1]is " << marray[1][1] <<endl;*/
+	cout<< "The array [1][1]is " << marray[1][1] <<endl;
 
 
 return 0;
