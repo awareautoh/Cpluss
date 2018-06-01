73453073b297dcd8722594adfc408c33f62a8e92
awareautoh <ouphachay@hotmail.com>
2018-06-01 19:22:53 +0100

    first commit

---
+++ recurs.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Recursive to reverse the array 
+
+void reverseArray(int *arpt, int lenght){
+
+	if(lenght == 0){
+
+		return;
+	}
+	else{
+
+		reverseArray(arpt+1, lenght-1);
+		cout<< *arpt <<endl;
+
+	}
+
+}
+
+
+int main(){
+
+	int array[] = {1,2,3,4,5,6};
+	reverseArray(array,6);
+	
+
+	return 0;
+}
